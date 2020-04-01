45773552c94c4fac3854acc4971e01e71d56d5e3
radsy <62997432+radsy@users.noreply.github.com>
2020-04-01 15:29:15 +0200

    Add files via upload

---
+++ Projet-MSPR/Projet-MSPR/src/main/java/fr/epsi/ProjetMSPR/ProjetMsprApplication.java
@@ -0,0 +1,13 @@
+package fr.epsi.ProjetMSPR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetMsprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetMsprApplication.class, args);
+	}
+
+}
